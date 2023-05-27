3f2dd1907895ca7d36aba770c625949c45fd88d0
Kulpot <kulpotwahu@gmail.com>
2023-05-28 05:13:42 +0800

    Add project files.

---
+++ Bring_window_to_foreground_and_give_it_focus/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bring_window_to_foreground_and_give_it_focus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Bring_window_to_foreground_and_give_it_focus/NativeMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+//Basically do it this way to actually give the window focus, even if another window in another app is in use.
+
+namespace Bring_window_to_foreground_and_give_it_focus
+{
+    static class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        // Make sure the program knows exacly. how to interpret the return type
+        [return: MarshalAs(UnmanagedType.Bool)]
+        // Mark public static, no need to be object oriented or private
+        // Mark extern to search outside the assembly, specifically in user32.dll
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+    }
+}
